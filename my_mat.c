3e84660d003c17a0423babb907f1e3b25e2efaf7
DavidOP1 <54214707+DavidOP1@users.noreply.github.com>
2021-11-23 09:34:52 +0200

    Add files via upload

---
+++ my_mat.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "my_mat.h"
+#include <limits.h> 
+#define size 10
+void assignValues(int graph[size][size])
+{
+int value;
+//int length = sizeof graph[0] / sizeof graph[0][0];
+for(int i=0;i<size;i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        printf("Assign value at position (%d,%d):\n",i,j);
+        scanf("%d" , &value);
+        graph[i][j]=value;
+    }
+    
+}
+}
+void checkRoute(int graph[size][size],int i,int j){
+   if(shortestRoute(graph,i,j)>0&&shortestRoute(graph,i,j)<INT_MAX) printf("True\n");
+   else printf("False\n");
+}
+int shortestRoute(int graph[size][size],int i,int j){
+    for(int i=0;i<size;i++){
+    for(int j=0;j<size;j++){
+        if(i!=j&&graph[i][j]==0) graph[i][j]=INT_MAX;
+    }
+}
+    //int sudo=0;
+    int matrix[size][size][size]={0};
+
+     for(int k=0;k<size;k++){
+         for(int i=0;i<size;i++){
+             for(int j=0;j<size;j++){
+                 if(k==0){
+                 if(i==0||j==0) matrix[k][i][j]=graph[i][j];
+                 if(i!=j){
+                    matrix[k][i][j]=((graph[i][j])-(graph[i][0]+graph[0][j])<=0)?graph[i][j]:(graph[i][0]+graph[0][j]);
+                 }} else{
+                   if(i==k-1||j==k-1)matrix[k][i][j]=matrix[k-1][i][j];
+                    if(i!=j){
+                        matrix[k][i][j]=((matrix[k-1][i][j])-(matrix[k-1][i][k]+matrix[k-1][k][j]));
+                    }
+                 }
+                 
+                 }
+         }
+     }
+    //  for(int i=0;i<size;i++)
+    //     {
+    //         for(int j=0;j<size;j++){
+    //             printf("%d\n",matrix[size-1][i][j]);
+    //         }
+    //     }
+    if(matrix[size-1][i][j]!=0){ return matrix[size-1][i][j];}
+    else { return 0;}
+}
